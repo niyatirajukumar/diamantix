1419a403ec165e7937e7e29b558c805f8415f1f1
Arnav Kumar <72879799+arnav-kr@users.noreply.github.com>
2024-10-17 13:20:50 +0000

    feat: registration ui

--- event/main.js
+++ event/main.js
@@ -1,3 +1,31 @@
 import "../globals.css";
 import "../forms.css";
 import "./local.css";
+
+document.querySelector("#register-btn").addEventListener("click", () => {
+  document.querySelector("#register-dialog").showModal();
+});
+
+document.querySelector("#connect-wallet").addEventListener("click", () => {
+  let walletId;
+  // connect wallet
+  // process
+  // if success show details form and hide connect wallet button
+  document.querySelector("#connect-wallet").classList.toggle("hidden", true);
+
+  // document.querySelector("#wallet-id").textContent = walletId;
+  document.querySelector("#additional-details").classList.toggle("hidden", false);
+});
+
+document.querySelector("#register-from").addEventListener("submit", (e) => {
+  e.preventDefault();
+  let formData = new FormData(e.target);
+  let data = {};
+  for (let [key, value] of formData.entries()) {
+    data[key] = value;
+  }
+  console.log(data); // { name, email }
+  // process
+  // if success, close dialog
+  document.querySelector("#register-dialog").close();
+});
