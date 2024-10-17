495a08a84562aa71034b033de376bd1fdf7d4e26
Niyati <niyati1015@gmail.com>
2024-10-17 21:36:49 +0530

    something

--- event/main.js
+++ event/main.js
@@ -82,3 +82,29 @@ document.querySelector("#register-from").addEventListener("submit", (e) => {
 function getEventId() {
   return new URLSearchParams(window.location.search).get("id");
 }
+
+// Get the modal
+var modal = document.getElementById("connectWalletModal");
+
+// Get the button that opens the modal (Register button)
+var registerBtn = document.getElementById("registerButton");
+
+// Get the <span> element that closes the modal
+var closeBtn = document.getElementsByClassName("close-button")[0];
+
+// When the user clicks on the button, open the modal
+registerBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
